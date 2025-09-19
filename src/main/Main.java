c7a1104873494eb66dcb4c6e0c8515261168e122
kim-1111 <em.huang.yixin@gmail.com>
2025-09-19 18:24:08 +0200

    add #1 boolean field to product

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,69 +5,74 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	/**
 	 * Leer un fichero y mostrarlo por consola
-	 * */
+	 */
 	public static void leerFichero(File f) throws IOException {
 		FileReader fr = new FileReader(f);
 		BufferedReader br = new BufferedReader(fr);
 		String miLinea = br.readLine();
-		while(miLinea != null) {
+		while (miLinea != null) {
 			System.out.println(miLinea);
 			miLinea = br.readLine();
 		}
 		fr.close();
-		br.close();	
+		br.close();
 	}
+
 	/**
 	 * Escribir un fichero en un array 5 objetos de la clase productos.
-	 * */
+	 */
 	public static void escribirFichero(File f) throws IOException {
-		//Se puede escribir en el fichero.
-		FileWriter fw = new FileWriter(f,true);
+		// Se puede escribir en el fichero.
+		FileWriter fw = new FileWriter(f, true);
 		PrintWriter pw = new PrintWriter(fw);
-		Scanner sc = new Scanner(System.in);
+
 		ArrayList<Producto> productos = new ArrayList<>();
-		for(int i=0; i<5; i++) {
-			System.out.println("Mi iteración: "+i);
+		for (int i = 0; i < 3; i++) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Mi iteración: " + i);
 			System.out.print("Introduzca Nombre del producto: ");
 			String nombreProducto = sc.next();
 			System.out.print("Introduzca Precio del producto: ");
 			float precio = sc.nextFloat();
 			System.out.print("Introduzca Descripción del producto: ");
 			String desc = sc.next();
-			//Añado elementos al array
-			productos.add(new Producto(precio,nombreProducto,desc));
-			//Escribo en el fichero la información de cada producto
+			// Añado elementos al array
+			productos.add(new Producto(precio, nombreProducto, desc, true));
+			// Escribo en el fichero la información de cada producto
 			pw.append(productos.get(i).toString());
 		}
 		pw.close();
 		fw.close();
-		
+
 	}
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		File f = new File(System.getProperty("user.dir")+File.separator+"resources/prueba.txt");
+		File f = new File(System.getProperty("user.dir") + File.separator + "resources/prueba.txt");
 		File newFolder = new File("resources");
-		//Comprobmos si existe el directorio y si no lo creamos
-		if(!newFolder.exists()){
+		// Comprobmos si existe el directorio y si no lo creamos
+		if (!newFolder.exists()) {
 			newFolder.mkdir();
 		}
-		//Ahora comprobamos si existe el archivo
-		if(!f.exists()) {
+		// Ahora comprobamos si existe el archivo
+		if (!f.exists()) {
 			f.createNewFile();
 		}
-		
-		if(f.canRead()) {
+
+		if (f.canRead()) {
 			leerFichero(f);
-		}else if(f.canWrite()) {
-			escribirFichero(f);
-		}else {
-			System.out.println("NO SE PUEDE ESCRIBIR EN EL ARCHIVO");
+			if (f.canWrite()) {
+				escribirFichero(f);
+			} else {
+				System.out.println("NO SE PUEDE ESCRIBIR EN EL ARCHIVO");
+			}
+		} else {
+			System.out.println("NO SE PUEDE LEER EN EL ARCHIVO");
 		}
-		
-		
+
 	}
 
 }
